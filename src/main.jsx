51b01fea926b85817dad0cb75030797566d40b1d
Cristiano <cristiano_bonsai@hotmail.com>
2026-06-05 20:10:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Login from './pages/Login.jsx'
+import Painel from './pages/Painel.jsx'
+import { AuthProvider, ProtectedRoute } from './auth/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/painel"
+            element={
+              <ProtectedRoute>
+                <Painel />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 )
